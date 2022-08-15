8d97824967ce5701962150e6a53c4e2c46a06b31
Hải Phạm <haipham.net@gmail.com>
2022-08-15 23:33:20 +0700

    config plugin

---
+++ shell/zsh.sh
@@ -0,0 +1,19 @@
+platform='unknow'
+unamestr=`uname`
+if [[ "$unamestr" == 'Darkwin' ]]; then
+    platform = 'macos'
+fi
+
+ZNAP_DIR=$HOME/dotfiles/plugins
+
+[[ -f '$ZNAP_DIR/znap.zsh' ]] ||
+    git clone --depth 1 -- \
+        https://github.com/marlonrichert/zsh-snap.git $ZNAP_DIR
+
+
+source $ZNAP_DIR/znap.zsh
+znap source marlonrichert/zsh-autocomplete
+znap source zsh-users/zsh-syntax-highlighting
+
+export STARSHIP_CONFIG=~/dotfiles/shell/framework/starship.toml
+eval "$(starship init zsh)"
