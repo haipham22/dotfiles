e539c2dae0463addbd992fbb9888756765376c5f
Hải Phạm <haipham.net@gmail.com>
2022-08-16 14:54:30 +0700

    fixbug if condition

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -6,12 +6,14 @@ fi
 
 ZNAP_DIR=$HOME/dotfiles/plugins/znap
 
-[[ -f '$ZNAP_DIR/znap.zsh' ]] ||
+if [ ! -f "$ZNAP_DIR/znap.zsh" ]; then
     git clone --depth 1 -- \
         https://github.com/marlonrichert/zsh-snap.git $ZNAP_DIR
 
+fi
 
-source $ZNAP_DIR/znap.zsh
+
+source "$ZNAP_DIR/znap.zsh"
 znap source marlonrichert/zsh-autocomplete
 znap source zsh-users/zsh-syntax-highlighting
 
