f85e8f239d9592e870cf4e48c5c0b80f02e42eb3
Hải Phạm <haipham.net@gmail.com>
2022-10-20 14:28:26 +0700

    add alias

--- shell/inc/alias.sh
+++ shell/inc/alias.sh
@@ -1,3 +1,19 @@
+
+# Tool
 alias pn=pnpm
+
+# Linux command
 alias rm='rm -rf'
 alias ll='ls -l'
+alias mkdir="mkdir -p"
+
+
+# git command
+alias gpl="git pull"
+alias gplb="git pull --rebase"
+alias gf="git diff"
+alias gst="git status"
+alias gcm="git commit"
+alias gcma="git commit --amend"
+alias gco="git checkout"
+alias gph="git push"
