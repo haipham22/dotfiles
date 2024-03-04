d21bdb261a15e8a2f269f7cc60028008c7a436ce
Hải Phạm <haipham.net@gmail.com>
2024-03-04 17:20:35 +0700

    add empty commit func

--- shell/inc/functions.sh
+++ shell/inc/functions.sh
@@ -2,3 +2,7 @@ function git_clean_branch() {
   echo "Delete local branch except $1"
   git branch | grep -v "$1" | xargs git branch -D
 }
+
+function git_empty_commit() {
+  git commit --allow-empty -m "Empty-Commit" && git push
+}
